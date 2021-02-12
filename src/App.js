9992b2f86207029ba64ec160a0a07fba14085952
Tian <tian.qiu142@gmail.com>
2021-02-11 22:57:28 -0800

    3rd attempt without notes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { useState } from "react"
 
 function App() {
+  const [custonNumber, setCustomNumber] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>1</h1>
+      <button>add 1</button>
+      <button>subtract 1</button>
+      <div>
+        <input value={custonNumber} onChange={setCustomNumber} />
+        <button>increment by input</button>
+        <button>increment asynchronously</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
