023a8f218d9ee90af5340b92a8573446dd70d08f
Tian <tian.qiu142@gmail.com>
2021-02-11 23:15:24 -0800

    completed, forgot to export actions as counterSlice.actions, and forgot to properly use onChange with e.target.value

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import "./App.css"
 import React, { useState } from "react"
+import {
+  increment,
+  decrement,
+  incrementBy,
+  incrementAsync,
+  selectCounter,
+} from "./counterSlice"
+
+import { useSelector, useDispatch } from "react-redux"
 
 function App() {
   const [custonNumber, setCustomNumber] = useState(0)
 
+  const number = useSelector(selectCounter)
+
+  const dispatch = useDispatch()
+
   return (
     <div>
-      <h1>1</h1>
-      <button>add 1</button>
-      <button>subtract 1</button>
+      <h1>{number}</h1>
+      <button onClick={() => dispatch(increment())}>add 1</button>
+      <button onClick={() => dispatch(decrement())}>subtract 1</button>
       <div>
-        <input value={custonNumber} onChange={setCustomNumber} />
-        <button>increment by input</button>
-        <button>increment asynchronously</button>
+        <input
+          value={custonNumber}
+          onChange={(e) => setCustomNumber(e.target.value)}
+        />
+        <button
+          onClick={() => dispatch(incrementBy(Number(custonNumber) || 0))}
+        >
+          increment by input
+        </button>
+        <button
+          onClick={() => dispatch(incrementAsync(Number(custonNumber) || 0))}
+        >
+          increment asynchronously
+        </button>
       </div>
     </div>
   )
